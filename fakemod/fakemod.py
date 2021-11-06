22b27447f78850022bda9e7fe209f55efb26c273
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-06 14:57:14 +0800

    testing gang

--- fakemod/fakemod.py
+++ fakemod/fakemod.py
@@ -16,7 +16,7 @@ class FakeMod(commands.Cog):
             {
                 "name": "bam",
                 "default_setting": True,
-                "image": "\N{FACE SCREAMING IN FEAR} \N{HAMMER}",
+                "image": "\N{FACE SCREAMING IN FEAR}",
                 "case_str": "Bam",
             },
             {
