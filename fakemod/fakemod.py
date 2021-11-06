933a031fe29197c8298413dccd2f1c50f7ea4130
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-06 14:43:33 +0800

    not done yet but commit test

--- fakemod/fakemod.py
+++ fakemod/fakemod.py
@@ -1,1 +1,41 @@
+from redbot.core import modlog, commands
+import discord
 
+
+class FakeMod(commands.Cog):
+    """Fake moderation commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    async def register_casetypes():
+        new_types = [
+
+            {
+                "name": "bam",
+                "default_setting": True,
+                "image": "\N{HAMMER}",
+                "case_str": "Bam",
+            },
+            {
+                "name": "kick",
+                "default_setting": True,
+                "image": "\N{WOMANS BOOTS}",
+                "case_str": "Kick",
+            }
+        ]
+        await modlog.register_casetypes(new_types)
+
+    @commands.command()
+    @commands.guild_only()
+    @checks.admin_or_permissions(ban_members=True)
+    async def bam(self, ctx, user: discord.Member, reason: str = None):
+        """Bams a user! 
+        """
+        case = await modlog.create_case(
+            ctx.bot, ctx.guild, ctx.message.created_at, action_type="ban",
+            user=user, moderator=ctx.author, reason=reason
+        )
+
+        await ctx.send(f"Banned {user}.")
