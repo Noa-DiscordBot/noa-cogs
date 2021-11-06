34408aa3c8255dad4dd01e72c642fde63528b73d
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-06 14:53:07 +0800

    scream

--- fakemod/fakemod.py
+++ fakemod/fakemod.py
@@ -16,7 +16,7 @@ class FakeMod(commands.Cog):
             {
                 "name": "bam",
                 "default_setting": True,
-                "image": "<:Ayaa:858201202017435659>",
+                "image": "\N{SCREAM}\N{HAMMER}",
                 "case_str": "Bam",
             },
             {
