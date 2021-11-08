529481e91b70d4ff28e1fab866eb84f532019a86
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-08 22:11:46 +0800

    oehehjrefgrgfrhgrueiuirebhruei

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -32,8 +32,8 @@ link_22="https://i.ibb.co/TLwFN5G/noa4starbirthday.png"
 link_23="https://i.ibb.co/mb2QRdz/noa4starbirthday-t.png"
 link_24="https://i.ibb.co/GCFH5mp/noaSP.png"
 
-        @commands.command()
-        async def randomnoa(self, ctx):
+    @commands.command()
+    async def randomnoa(self, ctx):
             noachoice = random.randint(1,2)
 
             if noachoice == 1:
@@ -46,5 +46,3 @@ link_24="https://i.ibb.co/GCFH5mp/noaSP.png"
                 e2.set_image=link_2
                 await ctx.send(embed=e2)
                 
-        
-        
