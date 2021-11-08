0f863bfdbed6d68deae39cf747635dce729a9dff
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-08 21:11:05 +0800

    owo

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -1,1 +1,10 @@
+from redbot.core import commands
+import random
 
+class RandomNoa(commands.Cog):
+    """Sends a random Noa card from the official D4DJ Groovy Mix game."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
