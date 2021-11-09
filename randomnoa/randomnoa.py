a85b7248015288a30ca3ab001a2fbeffa147590b
Tom E <guacaplushy@icloud.com>
2021-11-08 20:43:21 -0500

    Update randomnoa.py

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -10,7 +10,7 @@ class RandomNoa(commands.Cog):
 
     @commands.command()
     async def randomnoa(self, ctx):
-        cm = new cog_manager.CogManager()
+        cm = cog_manager.CogManager()
         cards = json.load(open(str(cm.install_path()) + "/randomnoa/cards.json", "r"))
         noachoice = random.randint(1, 2)
 
