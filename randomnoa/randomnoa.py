89ceac2c71c460cde3c098cb0082542bf42e139b
Tom E <guacaplushy@icloud.com>
2021-11-08 20:47:13 -0500

    Update randomnoa.py

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -17,10 +17,10 @@ class RandomNoa(commands.Cog):
 
         if noachoice == 1:
             embed=discord.Embed(title="Random Noa generated!", description="Card Name: Uniform \n Rarity: 1 ⭐ \n Untrained or Trained: None \n ", color=0x581919)
-            embed.set_image(cards[noachoice])
+            embed.set_image(url=cards[noachoice])
             await ctx.send(embed=embed)
         
         else:
             e=discord.Embed(title="Random Noa generated!", description="Card Name: DJ- LIVE! \n Rarity: 2 ⭐\n Untrained or Trained: None \n ", color=0x581919)
-            e.set_image(cards[noachoice])
+            e.set_image(url=cards[noachoice])
             await ctx.send(embed=e)
