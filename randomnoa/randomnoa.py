2030d9443c14c2916f31bb01436584f13a43e1be
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-09 10:29:39 +0800

    eh

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -16,11 +16,29 @@ class RandomNoa(commands.Cog):
         noachoice = random.randint(1, 2)
 
         if noachoice == 1:
-            embed=discord.Embed(title="Random Noa generated!", description="Card Name: Uniform \n Rarity: 1 ⭐ \n Untrained or Trained: None \n ", color=0x581919)
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: Uniform \n Rarity: 1⭐ \n Untrained or Trained: None \n ", color=await ctx.embed_color())
+            embed.set_image(url=cards[noachoice-1])
+            await ctx.send(embed=embed)
+
+        elif noachoice == 2:
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: DJ-LIVE! \n Rarity: 2⭐\n Untrained or Trained: None \n ", color=await ctx.embed_color())
+            embed.set_image(url=cards[noachoice-1])
+            await ctx.send(embed=embed)
+
+        elif noachoice == 3:
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: DJ-MIX! \n Rarity: 2⭐\n Untrained or Trained: None \n ", color=await ctx.embed_color())
+            embed.set_image(url=cards[noachoice-1])
+            await ctx.send(embed=embed)
+
+        elif noachoice == 4:
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: CuteBringer \n Rarity: 3⭐\n Untrained or Trained: Untrained \n ", color=await ctx.embed_color())
             embed.set_image(url=cards[noachoice-1])
             await ctx.send(embed=embed)
         
-        else:
-            embed=discord.Embed(title="Random Noa generated!", description="Card Name: DJ- LIVE! \n Rarity: 2 ⭐\n Untrained or Trained: None \n ", color=0x581919)
+        elif noachoice == 5:
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: CuteBringer \n Rarity: 3⭐\n Untrained or Trained: Trained \n ", color=await ctx.embed_color())
             embed.set_image(url=cards[noachoice-1])
             await ctx.send(embed=embed)
+
+        else:
+            await ctx.send("temp message OwO")
