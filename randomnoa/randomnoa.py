09466d09f1889c8077edefdab6594fc5534409a8
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-09 09:12:09 +0800

    Update randomnoa.py

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -44,5 +44,5 @@ class RandomNoa(commands.Cog):
         
         else:
             embed=discord.Embed(title="Random Noa generated!", description="Card Name: DJ- LIVE! \n Rarity: 2 ⭐\n Untrained or Trained: None \n ", color=0x581919)
-            embed.set_image=f"{link_2}"
+            embed.set_image=link_1
             await ctx.send(embed=embed)
