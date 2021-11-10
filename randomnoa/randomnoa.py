8a7aea3cdf98a1bff87300145bf759d7ad1b275f
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-10 22:09:28 +0800

    updated with rarity and whatnot

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -76,7 +76,7 @@ class RandomNoa(commands.Cog):
             await ctx.send(embed=embed)
 
         elif noachoice == 11:
-            embed=discord.Embed(title="Random Noa generated!", description="Card Name: i- \n Rarity: ok\n Untrained or Trained: Untrained \n ", color=await ctx.embed_color())
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: Enchanted Lamp \n Rarity: 3⭐\n Untrained or Trained: Trained  \n ", color=await ctx.embed_color())
             embed.set_image(url=cards[noachoice-1])
             embed.set_footer(text=noachoice)
             await ctx.send(embed=embed)
