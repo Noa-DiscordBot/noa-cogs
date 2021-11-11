5ccb9af236ff819f659aca3fc82a612213dbb5fe
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-11-11 15:43:30 +0800

    time to lock the cog again

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -13,7 +13,7 @@ class RandomNoa(commands.Cog):
         cm = cog_manager.CogManager()
         ipath = str(await cm.install_path())
         cards = json.load(open(ipath + "/randomnoa/cards.json", "r"))
-        noachoice = random.randint(1,13)
+        noachoice = 14 #random.randint(1,13)
 
         if noachoice == 1:
             embed=discord.Embed(title="Random Noa generated!", description="Card Name: Uniform \n Rarity: 1⭐ \n Untrained or Trained: None \n ", color=await ctx.embed_color())
@@ -88,6 +88,12 @@ class RandomNoa(commands.Cog):
             await ctx.send(embed=embed)
 
         elif noachoice == 13:
+            embed=discord.Embed(title="Random Noa generated!", description="Card Name: Skydrifter \n Rarity: 4⭐\n Untrained or Trained: Trained  \n ", color=await ctx.embed_color())
+            embed.set_image(url=cards[noachoice-1])
+            embed.set_footer(text=noachoice)
+            await ctx.send(embed=embed)
+
+        elif noachoice == 14:
             embed=discord.Embed(title="Random Noa generated!", description="Card Name: Skydrifter \n Rarity: 4⭐\n Untrained or Trained: Trained  \n ", color=await ctx.embed_color())
             embed.set_image(url=cards[noachoice-1])
             embed.set_footer(text=noachoice)
