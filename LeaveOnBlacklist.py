d64258e86e88c94646d1dcba204b84548b5028f7
Tom E <guacaplushy@icloud.com>
2021-11-20 21:24:28 -0500

    Create LeaveOnBlacklist.py

---
+++ LeaveOnBlacklist.py
@@ -0,0 +1,12 @@
+from redbot.core.commands import *
+
+class LeaveOnBlacklist(Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @Cog.listener()
+  async def on_guild_join(self, guild):
+    is_blacklisted = self.bot.allowed_by_whitelist_blacklist(who_id=guild.owner_id)
+    if is_blacklisted:
+      guild.leave()
