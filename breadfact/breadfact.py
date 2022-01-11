a9a6177617c339bd0d1a58e9b5d5f0b5b88b7b15
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2022-01-11 21:51:17 +0800

    Update breadfact.py

--- breadfact/breadfact.py
+++ breadfact/breadfact.py
@@ -1,1 +1,13 @@
+from redbot.core import commands
 
+class MyCog(commands.Cog):
+    """Bread facts!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def breadfact(self, ctx):
+        """Gets bread facts randomly :-)"""
+        # Your code will go here
+        await ctx.send()
