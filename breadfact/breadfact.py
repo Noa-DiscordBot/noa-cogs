c2a2d6c18c9dd0bfc17f4b64cdf65cd100c5fa1f
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-01-23 13:58:12 +0800

    ae

--- breadfact/breadfact.py
+++ breadfact/breadfact.py
@@ -12,8 +12,4 @@ class BreadFact(commands.Cog):
         ipath = str(await cm.install_path())
         facts = json.load(open(ipath + "/breadfact/facts.json", "r", encoding="utf-8"))
         bfint = random.randint(0,57)
-        if ctx.author.id == 750325757024141392 or ctx.author.id == 818631385392218112:
-            await ctx.send(facts[bfint])
-        elif bfint == 8 or bfint == 15 or bfint == 27 or bfint == 36 or bfint == 48 or bfint == 57:
-            await ctx.send(facts[bfint+1])
-
+        await ctx.send(facts[bfint])
