01f4b1326104c11847801c5a3f31ec9dd78f102f
Onii-chan <69662328+Onii-Chan-Discord@users.noreply.github.com>
2022-03-06 18:58:36 +1100

    Delete unused code.
    Since the commented code isn't needed, I've deleted it.

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -16,7 +16,6 @@ class RandomNoa(commands.Cog):
     async def random_noa(self):
         cm = cog_manager.CogManager()
         ipath = str(await cm.install_path())
-        # cards = json.load(open(ipath + "/randomnoa/cards.json", "r"))
         with open(ipath + "/randomnoa/cards.json", "r",  encoding="utf-8") as noa:
             data = json.load(noa)
             noas = data["noas"]
@@ -30,11 +29,9 @@ class RandomNoa(commands.Cog):
 
     @commands.command()
     async def randomnoa(self, ctx):
-        """Sends a random Noa card from the official D4DJ Groovy Mix game. (JP version.)"""
-        # cm = cog_manager.CogManager()
-        # ipath = str(await cm.install_path())
-        # cards = json.load(open(ipath + "/randomnoa/cards.json", "r"))
-        # noachoice = random.randint(1, 29)
+        """
+        Sends a random Noa card from the official D4DJ Groovy Mix game. (JP version.)
+        """
 
         data =  await self.random_noa()
         embed = discord.Embed(
