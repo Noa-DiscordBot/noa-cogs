7152c6f69a4c34fa5394c7330346db3832a559fd
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-23 21:59:00 +0800

    I dont know how this works, lol. lets try

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -1,4 +1,4 @@
-from redbot.core import commands, cog_manager
+from redbot.core import commands, cog_manager, Config
 import random
 import discord
 import json
@@ -12,6 +12,9 @@ class RandomNoa(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=694641)
+        default_guild = {"rigged": False, "cards": ""}
+        self.config.register_guild(**default_guild)
 
     async def random_noa(self):
         cm = cog_manager.CogManager()
@@ -19,12 +22,22 @@ class RandomNoa(commands.Cog):
         with open(ipath + "/randomnoa/cards.json", "r", encoding="utf-8") as noa:
             data = json.load(noa)
             noas = data["noas"]
-            random_index = str(random.randint(1, len(noas)))
-            return_data = {
-                "title": noas[random_index]["title"],
-                "url": noas[random_index]["image_url"],
-                "desc": noas[random_index]["desc"],
-            }
+            rig_val = await self.config.guild(ctx.guild).rigged()
+            card_no = await self.config.guild(ctx.guild).cards()
+            if await bot.is_owner(ctx.author) == True and rig_val == True:
+                index = str(card_no)
+                return_data = {
+                    "title": noas[index]["title"],
+                    "url": noas[index]["image_url"],
+                    "desc": noas[index]["desc"],
+                }
+            else:
+                random_index = str(random.randint(1, len(noas)))
+                return_data = {
+                    "title": noas[random_index]["title"],
+                    "url": noas[random_index]["image_url"],
+                    "desc": noas[random_index]["desc"],
+                }
             return return_data
 
     @commands.command()
@@ -44,3 +57,28 @@ class RandomNoa(commands.Cog):
             await ctx.reply(embed=embed, mention_author=False)
         except discord.HTTPException:
             await ctx.send(embed=embed)
+
+    @commands.is_owner()
+    @commands.group()
+    async def randomnoaset(self, ctx):
+        """Setup RandomNoa."""
+        pass
+    
+    @randomnoaset.command()
+    async def rigged(self, ctx, rig: bool):
+        """Toggle the rigged random noa."""
+        await self.config.guild(ctx.guild).rigged.set(rig)
+        await ctx.send("The new value has been set.")
+    
+    @randomnoaset.command()
+    async def rigged(self, ctx, card: str):
+        """Choose the rigged card"""
+        await self.config.guild(ctx.guild).cards(card)
+        await ctx.send("The new card value has been set.")
+
+    @randomnoaset.command()
+    async def view(self, ctx):
+        """View configuration settings, will make nicer when it actually works"""
+        riggedval = await self.config.guild(ctx.guild).chances()
+        cardno = await self.config.guild(ctx.guild).cards()
+        await ctx.send(f"```Rigged?: {riggedval}\n\n Card No: {cardno}```")
