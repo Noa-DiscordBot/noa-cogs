142437ddd0dd01b21625a77944509c501fe0390d
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-25 22:07:57 +0800

    Make it set for cards

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -78,7 +78,7 @@ class RandomNoa(commands.Cog):
     @randomnoaset.command()
     async def riggedcard(self, ctx, card: int):
         """Choose the rigged card"""
-        await self.config.card(card)
+        await self.config.card.set(card)
         await ctx.send("The new card value has been set.")
         
     @commands.is_owner()
