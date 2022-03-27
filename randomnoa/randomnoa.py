7cdb405fd3e0576b2b0223e27c8f7588681a1478
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-27 18:57:52 +0800

    Black Formatting

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -76,10 +76,10 @@ class RandomNoa(commands.Cog):
         """Choose the rigged card"""
         with open(bundled_data_path(self) / "cards.json", "r", encoding="utf-8") as noa:
             data = json.load(noa)
-            noas = data["noas"]    
+            noas = data["noas"]
         if card > len(noas) or card < len(noas):
             return await ctx.send(f"The value cannot be less then 1 or more then {len(noas)}")
-        else:    
+        else:
             await self.config.card.set(card)
         await ctx.send("The new card value has been set.")
 
