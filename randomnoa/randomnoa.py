3bb61568b913e4eaa1b38107ca300091b90360f6
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-09-06 21:08:30 +0800

    version 2 real push

--- randomnoa/randomnoa.py
+++ randomnoa/randomnoa.py
@@ -20,7 +20,7 @@ class RandomNoa(commands.Cog):
     """Sends a random Noa card from the official D4DJ Groovy Mix game."""
 
     __author__ = ["JeffJrShim, Onii-Chan"]
-    __version__ = "2.0.0-beta"
+    __version__ = "2.0.0"
 
     def __init__(self, bot):
         self.bot = bot
